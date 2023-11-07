8533f41f79879163af01b179bbe4c8e2bed6a2f7
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-07 17:41:40 +0200

    done

--- src/app/[_id]/page.tsx
+++ src/app/[_id]/page.tsx
@@ -1,5 +1,8 @@
 import { getProducts } from "@/helpers";
 import { ProductType } from "../../../type";
+import Container from "@/components/Container";
+import Image from "next/image";
+import FormatedPrice from "@/components/FormatedPrice";
 
 type Props = {
     searchParams: { [key: string]: string | string[] | undefined };
@@ -19,7 +22,64 @@ const ProductPage = async ({searchParams}: Props) => {
     console.log(products);
 
   return (
-    <div>зфпу</div>
+    <Container
+    className="flex items-center flex-col md:flex-row px-4"
+    >
+        <div
+        className="w-full md:w-1/2 overflow-hidden bg-zinc-50 flex items-center justify-center p-10"
+        >
+            <Image 
+            src={product?.image} 
+            className="transform transition-transform hover:scale-110 duration-500"
+            alt="product image" 
+            width={500} 
+            height={500} 
+            />
+        </div>
+        <div
+        className="w-full md:w-1/2 flex flex-col gap-2"
+        >
+            <h2
+            className="text-3xl font-semibold"
+            >
+                {product?.title}
+            </h2>
+            <p
+            className="flex items-center gap-10"
+            >
+                <FormatedPrice 
+                amount={product?.price}
+                className="text-lg font-semibold"
+                />
+                <FormatedPrice 
+                amount={product?.previousPrice}
+                className="text-zinc-500 line-through"
+                />
+            </p>
+            <p>
+            You saved {" "} 
+            <FormatedPrice 
+            amount={product?.previousPrice - product?.price}
+            className="text-base font-semibold bg-designColor underline underline-offset-2"
+            />
+            {" "}
+            from this product.
+            </p>
+            <button
+            className="bg-designColor/80 text-zinc-700 px-6 py-2 
+            font-medium rounded-md hover:bg-designColor
+            hover:text-black cursor-pointer duration-200 hove:shadow-lg w-40 my-2"
+            >
+                add to cart
+            </button>
+            {
+                product?.isNew && (<p className="text-designColor font-semibold">New Arrival</p>)
+            }
+            <p>Brand: <span className="font-semibold">{product?.brand}</span></p>
+            <p>Category: <span className="font-semibold">{product?.brand}</span></p>
+            <p>{product?.description}</p>
+        </div>
+    </Container>
   )
 }
 
