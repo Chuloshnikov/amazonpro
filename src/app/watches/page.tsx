ebe10f5e35ff3f580bd97da65deba24014a7bfbe
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-08 16:11:45 +0200

    done

---
+++ src/app/watches/page.tsx
@@ -0,0 +1,26 @@
+import Product from '@/components/Product';
+import { getWatches } from '@/helpers';
+import Container from '../../components/Container';
+import React from 'react';
+
+const PhonesCasesPage = async () => {
+  const products = await getWatches();
+  return (
+    <Container>
+        <div
+        className='border-b-[1px] border-b-zinc-400 pb-4 flex items-center justify-between'
+        >
+          <h2>Watches</h2>
+          <p>Get the Watch you want</p>
+        </div>
+        <p
+        className='mt-4 text-zinc-500 font-semibold'
+        >
+          Showing all {products.length} results
+        </p>
+        <Product products={products}/>
+    </Container>
+  )
+}
+
+export default PhonesCasesPage;
