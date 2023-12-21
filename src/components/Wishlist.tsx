3092ea996f143662ea5197a575e8b3c2ca2418a9
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-12-21 21:03:05 +0200

    done (all to cart fix need)

--- src/components/Wishlist.tsx
+++ src/components/Wishlist.tsx
@@ -2,12 +2,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ProductType, StateProps } from '../../type';
-import { Minus, Plus, X } from "lucide-react";
+import { Minus, Plus, X, CheckCircle } from "lucide-react";
 import {
-    decreaseQuantity,
-    deleteProduct,
-    increaseQuantity,
-    resetCart,
+    addToCart,
+    addAllToCart,
+    deleteFavorite,
+    resetFavorite,
   } from "@/redux/proSlice";
 import toast, { Toaster } from "react-hot-toast";
 import Image from "next/image";
@@ -19,22 +19,21 @@ import {loadStripe} from '@stripe/stripe-js';
 import { useSession } from "next-auth/react";
 
 
-const Cart = () => {
+const Wishlist = () => {
     const [totalAmt, setTotalAmt] = useState(0);
     const [rowPrice, setRowPrice] =useState(0);
-    const { productData } = useSelector((state: StateProps) => state.pro);
+    const { productData, favoriteData } = useSelector((state: StateProps) => state.pro);
     const dispatch = useDispatch();
     const router = useRouter();
     const { data: session } = useSession();
-    console.log(productData)
 
     const handleReset = () => {
         const confirmReset = window.confirm(
-            "Are you sure you want to reset your Cart?"
+            "Are you sure you want to reset your Wishlist?"
         );
         if (confirmReset) {
-            dispatch(resetCart());
-            toast.success('Cart Reset Successfully');
+            dispatch(resetFavorite());
+            toast.success('Wishlist Reset Successfully');
             router.push("/");
         }
     };
@@ -43,48 +42,35 @@ const Cart = () => {
     useEffect(() => {
         let amt = 0;
         let rowAmt = 0;
-        productData.map((item:ProductType) => {
+        favoriteData.map((item:ProductType) => {
             amt += item.price * item.quantity;
             return;
         });
-        productData.map((item: ProductType) => {
+        favoriteData.map((item: ProductType) => {
             rowAmt += item?.previousPrice * item?.quantity;
         });
         setTotalAmt(amt);
         setRowPrice(rowAmt);
-    }, [productData]);
+    }, [favoriteData]);
 
-    // Stripe Payment
-    const stripePromise = loadStripe(
-        process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-    );
-    const handleCheckout = async () => {
-        const stripe = await stripePromise;
-        const response = await fetch("http://localhost:3000/api/checkout", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            items: productData,
-            email: session?.user?.email,
-          }),
-        });
-        const data = await response.json();
+
+    const handleToCart = async (item) => {
+        dispatch(addToCart(item));
+        dispatch(deleteFavorite(item?._id));
+    }
+
     
-        if (response.ok) {
-          // await dispatch(saveOrder({ order: productData, id: data.id }));
-          stripe?.redirectToCheckout({ sessionId: data.id });
-          // dispatch(resetCart());
-        } else {
-          throw new Error("Failed to create Stripe Payment");
-        }
+    const handleAllToCart = () => {
+        dispatch(addAllToCart(favoriteData));
+        dispatch(resetFavorite());
       };
  
   return (
     <div
     className='min-h-[465px]'
     >
-        {productData.length > 0 ? (
-        <div className='mt-5 flex flex-col'>
+        {favoriteData.length > 0 ? (
+        <div className='mt-5 flex flex-col min-h-[520px]'>
             <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
                 <table className="w-full text-sm text-left">
                     <thead className='text-xs text-white uppercase bg-zinc-950'>
@@ -96,17 +82,17 @@ const Cart = () => {
                                 Unit Price
                             </th>
                             <th scope='col' className='px-6 py-3'>
-                                Quantity
-                            </th>
-                            <th scope='col' className='px-6 py-3'>
-                                SubTotal
+                                Old price
                             </th>
                             <th scope='col' className='px-6 py-3'>
                                 Saving
                             </th>
+                            <th scope='col' className='px-6 py-3'>
+                                Add to cart
+                            </th>
                         </tr>
                     </thead>
-                    {productData.map ((item:ProductType) => (
+                    {favoriteData.map ((item:ProductType) => (
                         <tbody key={item?._id}>
                             <tr className="bg-white border-b-[1px] border-b-zinc-300">
                                 <th  
@@ -114,7 +100,7 @@ const Cart = () => {
                                 className="px-6 py-4 flex items-center gap-3"
                                 >
                                     <X 
-                                    onClick={() => {dispatch(deleteProduct(item?._id)), 
+                                    onClick={() => {dispatch(deleteFavorite(item?._id)), 
                                         toast.success(
                                             `${item.title} is removed from Wishlist!`
                                         );
@@ -135,38 +121,19 @@ const Cart = () => {
                                 <td className="px-6 py-4">
                                     <FormatedPrice amount={item?.price} />
                                 </td>
-                                <td className="px-6 py-4 flex items-center gap-4">
-                                    <span className="border border-zinc-300 p-1 rounded-md hover:border-zinc-800 cursor-pointer duration-200 inline-flex items-center justify-center">
-                                        <Minus
-                                        onClick={() =>
-                                            item?.quantity > 1
-                                            ? dispatch(decreaseQuantity(item)) &&
-                                                toast.success(
-                                                "Quantity decreased Successfully!"
-                                                )
-                                            : toast.error("Can not delete less than 1")
-                                            }
-                                        className="w-4 h-4"
-                                        />
-                                    </span>
-                                    <span className="font-semibold">{item?.quantity}</span>
-                                    <span className="border border-zinc-300 p-1 rounded-md hover:border-zinc-800 cursor-pointer duration-200 inline-flex items-center justify-center">
-                                        <Plus
-                                        onClick={() => {
-                                            dispatch(increaseQuantity(item)),
-                                            toast.success(`${item?.title} quantity added`);
-                                        }}
-                                        className="w-4 h-4"
-                                        />
-                                    </span>
-                                </td>
                                 <td className="px-6 py-4">
-                                    <FormatedPrice amount={item?.price * item?.quantity} />
+                                <FormatedPrice amount={item?.previousPrice}/>
                                 </td>
                                 <td className="px-6 py-4">
                                     <p className='bg-zinc-900 w-20 text-sm font-semibold text-center text-white py-1 rounded-md'>
                                         {calculatePercentage(item?.price, item?.previousPrice)}%{" "}save
                                     </p>
+                                </td>
+                                <td className="px-6 py-4 flex">
+                                    <CheckCircle
+                                    onClick={() => handleToCart(item)}
+                                    className='ml-7 hover:text-designColor duration-200 cursor-pointer'
+                                    />
                                 </td>
                             </tr> 
                         </tbody>
@@ -178,14 +145,14 @@ const Cart = () => {
             className='bg-zinc-950 text-zinc-200 w-36 py-3 mt-5 
             rounded-md uppercase text-xs font-semibold hover:bg-red-700
              hover:text-white duration-200'>
-                Reset Cart
+                Reset wishlist
             </button>
             <div className='mt-4 bg-white max-w-xl p-4 flex flex-col gap-1'>
                 <p className='border-b-[1px] border-b-designColor py-1'>
                     Cart Summary
                 </p>
                 <p className='flex items-center justify-between'>
-                    Total Items <span>{productData.length}</span>
+                    Total Items <span>{favoriteData.length}</span>
                 </p>
                 <p className='flex items-center justify-between'>
                     Price{" "} <span><FormatedPrice amount={rowPrice}/></span>
@@ -197,17 +164,17 @@ const Cart = () => {
                     Total Price{" "} <span><FormatedPrice amount={totalAmt}/></span>
                 </p>
                 <button
-                onClick={handleCheckout}
+                onClick={handleAllToCart}
                 className='bg-zinc-800 text-zinc-200 my-2 py-2 uppercase text-center 
                 rounded-md font-semibold hover:bg-black hover:text-white duration-200'
                 >
-                    Proceed to Checkout
+                    Add all to cart
                 </button>
             </div>
         </div>
         ) : (
         <div className='py-10 flex flex-col gap-1 items-center justify-center'>
-            <p className='text-lg font-bold'>Your Cart is Empty</p>
+            <p className='text-lg font-bold'>Your Wishlist is Empty</p>
             <Link 
             className="text-sm uppercase font-semibold underline underline-offset-2 hover:text-designColor duration-200 cursor-pointer"
             href={"/"}
@@ -228,4 +195,4 @@ const Cart = () => {
   )
 }
 
-export default Cart;
+export default Wishlist;
